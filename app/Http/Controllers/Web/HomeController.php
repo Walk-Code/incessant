c6b3d808fb941deea04384102dd73b900f8e1e73
walkcode <walk_code@163.com>
2017-05-02 11:28:06 +0800

    add paginate

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('web.home',['pages' => Page::get()]);
+        return view('web.home',['pages' => Page::paginate(15)]);
     }
 }
